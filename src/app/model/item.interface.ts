1c47cce44be65e1342403c1b8e4c27c6e604d1cf
lance <lance.brown42@gmail.com>
2021-10-27 18:42:24 -0400

    init app

---
+++ src/app/model/item.interface.ts
@@ -0,0 +1,7 @@
+export interface PantryItem {
+    id: number;
+    sku?: string;
+    description: string;
+    quantity: number;
+    img?: string;
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,26 @@
+/* eslint-disable guard-for-in */
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+import { PantryItem } from '../model/item.interface';
+import * as PantryJSON from '../../../db/items.json';
+// import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
+let inventory: PantryItem[];
+const source: PantryItem[] = JSON.parse(JSON.stringify(PantryJSON));
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
-  constructor() {}
+  constructor(public photoService: PhotoService) { }
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+  populateInventory(){
+    // let pantryFactory: PantryItem;
+    // if(source){
+    // }
+  }
 
 }
