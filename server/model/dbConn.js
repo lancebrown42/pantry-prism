6dceb6283dab07737cb9160e3ed24acf9fbd7d60
lance <lance.brown42@gmail.com>
2021-11-03 01:40:11 -0400

    added express and established db connection

---
+++ server/model/dbConn.js
@@ -0,0 +1,44 @@
+var Connection = require('tedious').Connection
+var Request = require('tedious').Request
+var env = require('../env.json');
+
+var config = env.db;
+
+
+var connection = new Connection(config)
+
+
+
+connection.on('connect', function (err) {
+
+  if (err) {
+    console.log(err)
+  } else {
+    executeStatement()
+  }
+})
+connection.connect();
+function executeStatement () {
+
+  request = new Request("select * from db_owner.TConnTest", function (err, rowCount) {
+      console.log(rowCount);
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(rowCount + ' rows')
+    }
+    connection.close()
+  })
+
+  request.on('row', function (columns) {
+    columns.forEach(function (column) {
+      if (column.value === null) {
+        console.log('NULL')
+      } else {
+        console.log(column.value)
+      }
+    })
+  })
+
+  connection.execSql(request)
+}
---
+++ server/routes/test.route.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const app = express();
+const testRoute = express.Router();
+const connection = require('tedious').Connection;
+const request = require('tedious').Request;
+
+
+
+testRoute.route('/').get((req, res)=>{
+    console.log('uhhhhhh slash');
+    res.json('bingo');
+});
+
+
+
+
+
+// userRoute.route('/').get((req, res) => {
+//   UserModel.find((error, user) => {
+//     if (error) {
+//       return next(error)
+//     } else {
+//       res.json(user)
+//       console.log('Users retrieved!')
+//     }
+//   })
+// })
+
+
+// userRoute.route('/create-user').post((req, res, next) => {
+//   UserModel.create(req.body, (err, user) => {
+//     if (err) {
+//       return next(err)
+//     } else {
+//       res.json(user)
+//       console.log('User created!')
+//     }
+//   })
+// });
+
+
+// userRoute.route('/fetch-user/:id').get((req, res) => {
+//   UserModel.findById(req.params.id, (err, user) => {
+//     if (err) {
+//       return next(err)
+//     } else {
+//       res.json(user)
+//       console.log('User retrieved!')
+//     }
+//   })
+// })
+
+
+// userRoute.route('/update-user/:id').put((req, res, next) => {
+//   UserModel.findByIdAndUpdate(req.params.id, {
+//     $set: req.body
+//   }, (err, user) => {
+//     if (err) {
+//       return next(err);
+//     } else {
+//       res.json(user)
+//       console.log('User updated!')
+//     }
+//   })
+// })
+
+// userRoute.route('/delete-user/:id').delete((req, res, next) => {
+//   UserModel.findByIdAndRemove(req.params.id, (error, user) => {
+//     if (error) {
+//       return next(error);
+//     } else {
+//       res.status(200).json({
+//         msg: user
+//       })
+//       console.log('User deleted!')
+//     }
+//   })
+// })
+
+module.exports = testRoute;
