a16a4465e2bbc07d016aa9588c7d396c95dd40d9
lance <lance.brown42@gmail.com>
2021-12-15 00:58:10 -0500

    heroku env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  server: 'http://localhost:3000/api/',
+  production: true,
+  server: 'https://pantryprism.net:3000/api/',
   mobileServer: 'https://pantryprism.net:3000/api/',
 };
 
